26724b5965d78932a14818913d4560f2f5f33748
Miguel <miguel@miguels-mbp.home>
2013-12-13 14:20:19 -0300

    add basic functions

--- src/model/Functions.java
+++ src/model/Functions.java
@@ -2,4 +2,24 @@ package model;
 
 public class Functions {
 
+	public void somar(double x, double y){
+
+		return x + y;
+	}
+
+	public void subtrair(double x, double y){
+
+		return x - y;
+	}
+
+	public void dividir(double x, double y){
+
+		return x / y;
+	}
+
+	public void multiplicar(double x, double y){
+
+		return x * y;
+	}
+
 }
