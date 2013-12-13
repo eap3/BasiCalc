12785f49716491944d1bc67c6ca79c0c6f580010
lfn2 <lfn2@cin.ufpe.br>
2013-12-13 14:58:56 -0300

    mudado functions

--- src/model/Functions.java
+++ src/model/Functions.java
@@ -2,24 +2,29 @@ package model;
 
 public class Functions {
 
-	public void somar(double x, double y){
+	public double somar(double x, double y){
 
 		return x + y;
 	}
 
-	public void subtrair(double x, double y){
+	public double subtrair(double x, double y){
 
 		return x - y;
 	}
 
-	public void dividir(double x, double y){
+	public double dividir(double x, double y){
 
 		return x / y;
 	}
 
-	public void multiplicar(double x, double y){
+	public double multiplicar(double x, double y){
 
 		return x * y;
+	}
+	
+	public double fatorial (double x){
+		if (x <= 1) return 1;
+		else return x * fatorial(x-1);
 	}
 
 }
